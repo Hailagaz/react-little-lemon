abacada56fdc505a756e13767088c48f3ec9f408
Hailagaz <omentielvo.lumenn@gmail.com>
2023-07-22 09:50:52 +0300

    fix

--- react-little-lemon/src/components/layout/Footer.js
+++ react-little-lemon/src/components/layout/Footer.js
@@ -16,9 +16,9 @@ import logoWhiteImage from './assets/logo-white.png';
 import pages from '../../utils/pages';
 
 const contacts = [
-  { icon: faLocationDot, info: '678 Pisa Ave, Chicago, IL 60611', },
-  { icon: faPhone, info: '(312) 593-2744', },
-  { icon: faEnvelope, info: 'customer@littlelemon.com', },
+	{ icon: faLocationDot, info: '221 Pennsylvania Ave.SE, Washington, DC 20003', },
+  { icon: faPhone, info: '(202) 547-9999', },
+  { icon: faEnvelope, info: 'order@littlelemon.com', },
 ];
 
 const socials = [
