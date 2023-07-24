624747987ee76474be4c75801b62dda61f831392
Hailagaz <omentielvo.lumenn@gmail.com>
2023-07-24 12:14:51 +0300

    fix

--- react-little-lemon/src/components/pages/Home/WeekSpecials.js
+++ react-little-lemon/src/components/pages/Home/WeekSpecials.js
@@ -1,50 +1,49 @@
 import { Link } from 'react-router-dom';
-import bruschettaImage from './assets/bruschetta.jpg';
 import greekSaladImage from './assets/greek-salad.jpg';
-import lemonDessertImage from './assets/lemon-dessert.jpg';
+import baklavaImage from './assets/baklava.jpg';
+import grilledOctopusImage from './assets/grilled-octopus.jpg';
 import './WeekSpecials.css';
 import pages from '../../../utils/pages';
 import MealCard from './MealCard';
 
 const meals = [
-  {
-    name: 'Greek Salad',
-    image: greekSaladImage,
-    price: '$12.99',
-    description: `The famous greek salad of crispy lettuce, peppers, olives and 
-      our Chicago style feta cheese, garnished with crunchy garlic and rosemary 
+	{
+		name: 'Greek Salad',
+		image: greekSaladImage,
+		price: '$9.99',
+		description: `The famous greek salad of crispy lettuce, peppers, olives and 
+      our Washington style feta cheese, garnished with crunchy garlic and rosemary 
       croutons.`,
-  },
-  {
-    name: 'Bruschetta',
-    image: bruschettaImage,
-    price: '$5.99',
-    description: `Our Bruschetta is made from grilled bread that has been 
-      smeared with garlic and seasoned with salt and olive oil.`,
-  },
-  {
-    name: 'Lemon Dessert',
-    image: lemonDessertImage,
-    price: '$7.99',
-    description: `This comes straight from grandma's recipe book, every last 
-      ingredient has been sourced and is as authentic as can be imagined.`,
-  },
+	},
+	{
+		name: 'Grilled Octopus',
+		image: grilledOctopusImage,
+		price: '$19.99',
+		description: `Along harbours, octopus is hung out to dry like washing – one of the
+		 iconic images of Greece. Grilled or marinated, they make a fine meze (appetiser), or main course stewed in wine.`,
+	},
+	{
+		name: 'Baklava',
+		image: baklavaImage,
+		price: '$4.99',
+		description: `The classic baklava that came from Greece and involves honey, filo and ground nuts.`,
+	},
 ];
 
 const WeekSpecials = () => {
-  return (
-    <section className="container grid week-specials">
-      <div className="week-specials-header">
-        <h2>This week specials!</h2>
-        <Link className="button-primary" to={pages.get('orderOnline').path}>
-          Online Menu
-        </Link>
-      </div>
-      {meals.map((meal, index) => 
-        <MealCard key={index} meal={meal} />
-      )}
-    </section>
-  );
+	return (
+		<section className="container grid week-specials">
+			<div className="week-specials-header">
+				<h2>This week specials!</h2>
+				<Link className="button-primary" to={pages.get('orderOnline').path}>
+					Online Menu
+				</Link>
+			</div>
+			{meals.map((meal, index) =>
+				<MealCard key={index} meal={meal} />
+			)}
+		</section>
+	);
 };
 
 export default WeekSpecials;
