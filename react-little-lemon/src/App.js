eb27001ccdedb46c54b8b1d4ca07886a4969361d
Oleh Cherniavskyi <94393608+Hailagaz@users.noreply.github.com>
2023-08-11 12:22:35 +0300

    fix

--- react-little-lemon/src/App.js
+++ react-little-lemon/src/App.js
@@ -1,58 +1,6 @@
-// import {
-//   Route, 
-//   Routes
-// } from 'react-router-dom';
-// import pages from './utils/pages';
-// import Layout from './components/layout/Layout';
-// import Home from './components/pages/Home';
-// import Bookings from './components/pages/Bookings';
-// import ConfirmedBooking from './components/pages/Bookings/ConfirmedBooking';
-// import NotFound from './components/pages/NotFound';
-// import UnderConstruction from './components/pages/UnderConstruction';
-
-// const App = () => {
-//   return (
-//     <>
-//       <Layout>
-//         <Routes>
-//           <Route path={pages.get('home').path} element={<Home />} />
-//           <Route 
-//             path={pages.get('about').path} 
-//             element={<UnderConstruction />} 
-//           />
-//           <Route 
-//             path={pages.get('menu').path} 
-//             element={<UnderConstruction />} 
-//           />
-//           <Route path={pages.get('bookings').path} element={<Bookings />} />
-//           <Route 
-//             path={pages.get('confirmedBooking').path} 
-//             element={<ConfirmedBooking />} 
-//           />
-//           <Route 
-//             path={pages.get('orderOnline').path} 
-//             element={<UnderConstruction />} 
-//           />
-//           <Route 
-//             path={pages.get('login').path} 
-//             element={<UnderConstruction />} 
-//           />
-//           <Route path="*" element={<NotFound />} />
-//         </Routes>
-//       </Layout>
-//     </>
-//   );
-// };
-
-// export default App;
-
-
-
-
 import {
-	BrowserRouter as Router,
-	Route,
-	Routes
+  Route, 
+  Routes
 } from 'react-router-dom';
 import pages from './utils/pages';
 import Layout from './components/layout/Layout';
@@ -63,37 +11,89 @@ import NotFound from './components/pages/NotFound';
 import UnderConstruction from './components/pages/UnderConstruction';
 
 const App = () => {
-	return (
-		<Router basename={process.env.NODE_ENV === 'production' ? '/react-little-lemon/' : '/'}>
-			<Layout>
-				<Routes>
-					<Route path={pages.get('home').path} element={<Home />} />
-					<Route
-						path={pages.get('about').path}
-						element={<UnderConstruction />}
-					/>
-					<Route
-						path={pages.get('menu').path}
-						element={<UnderConstruction />}
-					/>
-					<Route path={pages.get('bookings').path} element={<Bookings />} />
-					<Route
-						path={pages.get('confirmedBooking').path}
-						element={<ConfirmedBooking />}
-					/>
-					<Route
-						path={pages.get('orderOnline').path}
-						element={<UnderConstruction />}
-					/>
-					<Route
-						path={pages.get('login').path}
-						element={<UnderConstruction />}
-					/>
-					<Route path="*" element={<NotFound />} />
-				</Routes>
-			</Layout>
-		</Router>
-	);
+  return (
+    <>
+      <Layout>
+        <Routes>
+          <Route path={pages.get('home').path} element={<Home />} />
+          <Route 
+            path={pages.get('about').path} 
+            element={<UnderConstruction />} 
+          />
+          <Route 
+            path={pages.get('menu').path} 
+            element={<UnderConstruction />} 
+          />
+          <Route path={pages.get('bookings').path} element={<Bookings />} />
+          <Route 
+            path={pages.get('confirmedBooking').path} 
+            element={<ConfirmedBooking />} 
+          />
+          <Route 
+            path={pages.get('orderOnline').path} 
+            element={<UnderConstruction />} 
+          />
+          <Route 
+            path={pages.get('login').path} 
+            element={<UnderConstruction />} 
+          />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Layout>
+    </>
+  );
 };
 
 export default App;
+
+
+
+
+// import {
+// 	BrowserRouter as Router,
+// 	Route,
+// 	Routes
+// } from 'react-router-dom';
+// import pages from './utils/pages';
+// import Layout from './components/layout/Layout';
+// import Home from './components/pages/Home';
+// import Bookings from './components/pages/Bookings';
+// import ConfirmedBooking from './components/pages/Bookings/ConfirmedBooking';
+// import NotFound from './components/pages/NotFound';
+// import UnderConstruction from './components/pages/UnderConstruction';
+
+// const App = () => {
+// 	return (
+// 		<Router basename={process.env.NODE_ENV === 'production' ? '/react-little-lemon/' : '/'}>
+// 			<Layout>
+// 				<Routes>
+// 					<Route path={pages.get('home').path} element={<Home />} />
+// 					<Route
+// 						path={pages.get('about').path}
+// 						element={<UnderConstruction />}
+// 					/>
+// 					<Route
+// 						path={pages.get('menu').path}
+// 						element={<UnderConstruction />}
+// 					/>
+// 					<Route path={pages.get('bookings').path} element={<Bookings />} />
+// 					<Route
+// 						path={pages.get('confirmedBooking').path}
+// 						element={<ConfirmedBooking />}
+// 					/>
+// 					<Route
+// 						path={pages.get('orderOnline').path}
+// 						element={<UnderConstruction />}
+// 					/>
+// 					<Route
+// 						path={pages.get('login').path}
+// 						element={<UnderConstruction />}
+// 					/>
+// 					<Route path="*" element={<NotFound />} />
+// 				</Routes>
+// 			</Layout>
+// 		</Router>
+// 	);
+// };
+
+// export default App;
